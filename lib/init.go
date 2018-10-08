45db3b6b7a2eed2072126c49cf901147160f03ad
Praful  Gupta <prafulgupta@Prafuls-MacBook-Air.local>
2018-10-08 22:39:50 +0530

    First commit

---
+++ lib/init.go
@@ -0,0 +1,7 @@
+package lib
+
+import "log"
+
+func Init() {
+	log.Println("********[INIT] lib/init.go**********")
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+
+	"test_docker/lib"
+)
+
+func main() {
+	log.Println("*******[INIT] app.go***********")
+
+	lib.Init()
+}
